0ad58e5c563be8b40e7fcacf2e4aa91db408635f
Davi Oliveira Sousa <100448775+DaviOliverFront@users.noreply.github.com>
2024-04-28 22:43:49 -0300

    coloquei o código
    primeira tentativa

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Frases do dia'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _numeroAleatorio = 0;
+  List _frases = [
+    'A persistência é o caminho do êxito!',
+    'O otimismo é a fé em ação!',
+    'Jesus é o caminho, a verdade e a vida!',
+    'Seja a mudança que o mundo precisa!'
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+  _numeroAleatorio = new Random().nextInt(4);
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Precione o botão " + " para gerar uma frase.',
+            ),
+            Text(
+              _frases[_numeroAleatorio],
+              style: TextStyle(fontSize: 22)
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
